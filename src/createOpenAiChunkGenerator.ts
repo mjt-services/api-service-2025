9148a8df3e5f27f78d40898f2ff0648bb8a5131b
Matt Taylor <matt@matthewjosephtaylor.com>
2025-04-01 10:36:10 -0500

    cleanup logging

--- src/createOpenAiChunkGenerator.ts
+++ src/createOpenAiChunkGenerator.ts
@@ -1,23 +1,24 @@
 import type { TextgenConnectionMap } from "@mjt-services/textgen-common-2025";
 import type { OpenAiChunk } from "./type/OpenAiChunk";
 
-
-export const createOpenAiChunkGenerator = (body: TextgenConnectionMap["textgen.generate"]["request"]["body"]) => (data: TextgenConnectionMap["textgen.generate"]["response"]): OpenAiChunk => {
-  return {
-    choices: [
-      {
-        delta: {
-          role: "assistant",
-          content: data.delta ?? "",
+export const createOpenAiChunkGenerator =
+  (body: TextgenConnectionMap["textgen.generate"]["request"]["body"]) =>
+  (data: TextgenConnectionMap["textgen.generate"]["response"]): OpenAiChunk => {
+    return {
+      choices: [
+        {
+          delta: {
+            role: "assistant",
+            content: data.delta ?? "",
+          },
+          index: 0,
+          finish_reason: null,
         },
-        index: 0,
-        finish_reason: null,
-      },
-    ],
-    id: "",
-    object: "chat.completion.chunk",
-    created: Date.now(),
-    model: body.model ?? "",
-    system_fingerprint: "fp_ollama",
+      ],
+      id: "",
+      object: "chat.completion.chunk",
+      created: Date.now(),
+      model: body.model ?? "",
+      system_fingerprint: "fp_ollama",
+    };
   };
-};
